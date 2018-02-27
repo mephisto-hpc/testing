1e8148595c2389a59a06cf8733e6507b6206ebe8
Bert Wesarg <bert.wesarg@tu-dresden.de>
2018-02-27 13:35:02 +0100

    more tests for array

--- mephisto/t/0001-array.cpp
+++ mephisto/t/0001-array.cpp
@@ -1,5 +1,7 @@
 #include <mephisto/array>
 
+#include <cassert>
+
 int
 main()
 {
@@ -7,6 +9,14 @@ main()
     arr3[0] = 0;
     arr3[1] = 1;
     arr3[2] = 2;
+
+    static_assert(arr3.size() == 3);
+    static_assert(!arr3.empty());
+    assert(arr3[0] == 0);
+    assert(arr3[1] == 1);
+    assert(arr3[2] == 2);
+
+    mephisto::array<int, 4> arr4({3, 2, 1, 0});
 
     return 0;
 }
