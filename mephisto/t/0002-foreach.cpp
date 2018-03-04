1c11101260a2793c5fde502b0417e9118a615c9c
Pascal Jungblut <pascal.jungblut@ifi.lmu.de>
2018-03-04 14:06:00 +0100

    Add meta object for the buffer

--- mephisto/t/0002-foreach.cpp
+++ mephisto/t/0002-foreach.cpp
@@ -1,32 +1,48 @@
-#include <mephisto/array>
-#include <mephisto/algorithm/for_each>
-// TODO: move from detail namespace
-#include <mephisto/detail/meta>
+#include <mephisto/buffer>
 #include <libdash.h>
+#include <alpaka/alpaka.hpp>
+
+
 
 int main(int argc, char *argv[]) {
     using Data = float; 
     using ArrT = dash::Array<Data>;
     using PatternT = typename dash::Array<Data>::pattern_type;
-    using MetaT = typename mephisto::detail::Metadata<PatternT>;
+    using MetaT = typename mephisto::Metadata<PatternT>;
     using ViewT = typename dash::Array<Data>::local_type;
     using Dim = alpaka::dim::DimInt<1>;
 
     dash::init(&argc, &argv);
- 
-    auto arr = dash::Array<Data>(1000);
 
+    // Setup a dash array and fill it
+    auto arr = dash::Array<Data>(1000);
+    dash::fill(arr.begin(), arr.end(), 5.0);
     using Size = decltype(arr.size());
 
-    using Acc = alpaka::acc::AccCpuSerial<Dim, Size>;
+    // Setup accelerator and host
+    using Acc       = alpaka::acc::AccCpuSerial<Dim, Size>;
+    using Host      = alpaka::acc::AccCpuSerial<Dim, Size>;
     using StreamAcc = alpaka::stream::StreamCpuSync;
 
-    using DevAcc = alpaka::dev::Dev<Acc>;
-    using PltfAcc = alpaka::pltf::Pltf<DevAcc>;
+    using DevAcc    = alpaka::dev::Dev<Acc>;
+    using DevHost   = alpaka::dev::Dev<Host>;
+    using PltfHost  = alpaka::pltf::Pltf<DevHost>;
+    using PltfAcc   = alpaka::pltf::Pltf<DevAcc>;
 
     DevAcc const devAcc(alpaka::pltf::getDevByIdx<PltfAcc>(0u));
+    DevHost const devHost(alpaka::pltf::getDevByIdx<PltfHost>(0u));
 
-    arr | dash::local();
+    // Context is a pair of host and accelerator used by the mpehisto::buffer
+    auto ctx = mephisto::make_ctx(devHost, devAcc);
 
-    /* auto buf = mephisto::detail::DataBuffer<Data, DevAcc, PatternT>(devAcc, arr | dash::local()...); */
+    // Get a local view to the array
+    auto local_arr = arr | dash::local();
+
+    // Create the host buffer. It can be used to copy data to the accelerator
+    // and returns a buffer on the device via getDeviceDataBuffer().
+    auto buf = mephisto::HostDataBuffer<
+      Data,
+      decltype(ctx),
+      PatternT,
+      decltype(local_arr)>(ctx, local_arr);
 }
