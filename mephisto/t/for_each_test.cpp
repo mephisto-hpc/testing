c591bea543bd5ecf8299cd210a3a9bed2336605f
Pascal Jungblut <pascal.jungblut@ifi.lmu.de>
2018-06-14 14:33:52 +0200

    Move for_each example to gtest

--- mephisto/t/for_each_test.cpp
+++ mephisto/t/for_each_test.cpp
@@ -1,15 +1,15 @@
+#include "gtest/gtest.h"
+#include "for_each_test.h"
 #include <libdash.h>
 #include <mephisto/algorithm/for_each>
 #include <mephisto/execution>
 #include <mephisto/array>
 #include <alpaka/alpaka.hpp>
 
+/* static int meph_argc; */
+/* static char** meph_argv; */
 
-struct Pos {
-    unsigned long x, y, z;
-};
-
-int main(int argc, char *argv[]) {
+TEST_F(ForEachTest, itWorks) {
   auto const Dim  = 3;
   using Data      = Pos;
   using PatternT  = dash::BlockPattern<Dim>;
@@ -19,31 +19,10 @@ int main(int argc, char *argv[]) {
   using ArrayT    = dash::Array<Data, dash::default_index_t, PatternT>;
   using SizeT     = ArrayT::size_type;
 
-  dash::init(&argc, &argv);
 
   PatternT pattern {10, 10, 10};
   ArrayT arr {pattern};
   dash::fill(arr.begin(), arr.end(), {42, 42, 42});
-
-
-    // Setup accelerator and host
-#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
-    using Acc       = alpaka::acc::AccGpuCudaRt<AlpakaDim, SizeT>;
-    using StreamAcc = alpaka::stream::StreamCudaRtSync;
-#else
-    using Acc       = alpaka::acc::AccCpuSerial<AlpakaDim, SizeT>;
-    using StreamAcc = alpaka::stream::StreamCpuSync;
-#endif
-    using Host      = alpaka::acc::AccCpuSerial<AlpakaDim, SizeT>;
-
-    using DevAcc    = alpaka::dev::Dev<Acc>;
-    using DevHost   = alpaka::dev::Dev<Host>;
-    using PltfHost  = alpaka::pltf::Pltf<DevHost>;
-    using PltfAcc   = alpaka::pltf::Pltf<DevAcc>;
-
-    // The mephisto kernel to use in the executor
-    using Kernel = mephisto::ForEachWithIndexKernel;
-
     DevAcc const devAcc(alpaka::pltf::getDevByIdx<PltfAcc>(0u));
     DevHost const devHost(alpaka::pltf::getDevByIdx<PltfHost>(0u));
     StreamAcc stream(devAcc);
@@ -60,24 +39,29 @@ int main(int argc, char *argv[]) {
     auto policy = mephisto::execution::make_parallel_policy(executor);
 
     // set the coordinates using an Alpaka policy
+    ForEachClb clb;
     dash::for_each_with_index(
         policy,
         arr.begin(),
         arr.end(),
-        [] ALPAKA_FN_ACC (
-            Data &data, const mephisto::array<SizeT, Dim> coords) {
-          data.x = coords[0];
-          data.y = coords[1];
-          data.z = coords[2];
-        });
+        clb
+        );
 
     // Check the written coordinates using the standard for_each_with_index
     dash::for_each_with_index(arr.begin(), arr.end(), [&pattern](const Data &d, PatternT::index_type i) {
         auto const coords = pattern.coords(i);
         printf("mephisto: [%lu,%lu,%lu] -  dash: [%lu,%lu,%lu]\n", d.x, d.y, d.z, coords[0], coords[1], coords[2]);
+        ASSERT_EQ(d.x, coords[0]);
+        ASSERT_EQ(d.y, coords[1]);
+        ASSERT_EQ(d.x, coords[2]);
     });
-    dash::finalize();
 }
 
 
+int main(int argc, char **argv) {
+  meph_argc = argc;
+  meph_argv = argv;
+  ::testing::InitGoogleTest(&argc, argv);
+  return RUN_ALL_TESTS();
+}
 
---
+++ mephisto/t/for_each_test.h
@@ -0,0 +1,69 @@
+#ifndef MEPHISTO_FOR_EACH_TEST_H
+#define MEPHISTO_FOR_EACH_TEST_H
+
+#include "gtest/gtest.h"
+#include <libdash.h>
+#include <mephisto/array>
+#include <mephisto/algorithm/for_each>
+
+int meph_argc;
+char** meph_argv;
+
+struct Pos {
+    unsigned long x, y, z;
+};
+
+using Data = Pos;
+
+class ForEachTest : public ::testing::Test {
+
+public:
+  static const int Dim        = 3;
+  using Data       = Pos;
+  using PatternT   = dash::BlockPattern<Dim>;
+  using MetaT      = typename mephisto::Metadata<PatternT>;
+  using ViewT      = typename dash::Array<Data>::local_type;
+  using AlpakaDim  = alpaka::dim::DimInt<1>;
+  using ArrayT     = dash::Array<Data, dash::default_index_t, PatternT>;
+  using SizeT      = ArrayT::size_type;
+
+// Setup accelerator and host
+#ifdef ALPAKA_ACC_GPU_CUDA_ENABLED
+  using Acc       = alpaka::acc::AccGpuCudaRt<AlpakaDim, SizeT>;
+  using StreamAcc = alpaka::stream::StreamCudaRtSync;
+#else
+  using Acc       = alpaka::acc::AccCpuSerial<AlpakaDim, SizeT>;
+  using StreamAcc = alpaka::stream::StreamCpuSync;
+#endif
+  using Host = alpaka::acc::AccCpuSerial<AlpakaDim, SizeT>;
+
+  using DevAcc   = alpaka::dev::Dev<Acc>;
+  using DevHost  = alpaka::dev::Dev<Host>;
+  using PltfHost = alpaka::pltf::Pltf<DevHost>;
+  using PltfAcc  = alpaka::pltf::Pltf<DevAcc>;
+
+  // The mephisto kernel to use in the executor
+  using Kernel = mephisto::ForEachWithIndexKernel;
+
+protected:
+  virtual void SetUp()
+  {
+    dash::init(&meph_argc, &meph_argv);
+  }
+
+  virtual void TearDown()
+  {
+    dash::finalize();
+  }
+};
+
+struct ForEachClb {
+  void operator()(Data &data, const mephisto::array<size_t, 3> coords)
+  {
+    data.x = coords[0];
+    data.y = coords[1];
+    data.z = coords[2];
+  }
+};
+
+#endif
